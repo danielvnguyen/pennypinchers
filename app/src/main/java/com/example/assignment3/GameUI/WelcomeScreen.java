32cc9b8d2588e63dcdc8b511753c0c21a9878a2d
ZaidArshad <zaidy.arshad@gmail.com>
2021-10-11 17:16:36 -0700

    Loading screen

--- app/src/main/java/com/example/assignment3/GameUI/WelcomeScreen.java
+++ app/src/main/java/com/example/assignment3/GameUI/WelcomeScreen.java
@@ -5,6 +5,7 @@ import com.example.assignment3.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -12,8 +13,17 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-
         Intent intent = new Intent(this, MainMenu.class);
-        startActivity(intent);
+
+        Handler loadingHandler = new Handler();
+
+        loadingHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
